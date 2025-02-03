2449133d235a53eaa687877d12845f8e927fdf23
ponce08 <yonathanponce2019@gmail.com>
2025-02-03 16:42:23 -0500

    protected path admin 2

--- src/App.tsx
+++ src/App.tsx
@@ -22,8 +22,8 @@ import { ProtectedRoute } from '@/utils/FunctionsDashboard.tsx';
 // import ImageUploader from '@/components/dashboard/Ensayo.tsx';
 
 const client = new ApolloClient({
-  // uri: 'https://ecommerce1-back.onrender.com/graphql',
-  uri: 'http://localhost:4000/graphql',
+  uri: 'https://ecommerce1-back.onrender.com/graphql',
+  // uri: 'http://localhost:4000/graphql',
   cache: new InMemoryCache()
 });
 
